b79e207ab746aaef95b11b58b628fc26f659e3aa
Gnzrg <baterdeneganzorig959@gmail.com>
2024-01-13 10:03:50 +0800

    F

--- src/components/Meta.tsx
+++ src/components/Meta.tsx
@@ -7,7 +7,7 @@ export const Meta = () => {
       <title>Эрч Гранд ХХК</title>
       <meta name="description" content="Erch Grand LLC 2024" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/ErchLogo.png" />
     </Head>
   );
 };
